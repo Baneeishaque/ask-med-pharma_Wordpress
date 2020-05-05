f792b49d5755295e194f1d7611f635fc046c6c43
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-05 16:33:52 +0530

    Ssquared Sub
    Idea Project too.

--- wp-config.php
+++ wp-config.php
@@ -18,15 +18,18 @@
  * @package WordPress
  */
 
+define('WP_HOME','https://askmedpharma.ssquared.qa');
+define('WP_SITEURL','https://askmedpharma.ssquared.qa');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp-4' );
+define( 'DB_NAME', 'ssquareq_askmedpharma' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ssquareq_dotlocus' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'aA9895204814' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
